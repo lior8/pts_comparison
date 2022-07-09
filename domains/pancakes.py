ee2bc711eb99b4f54b143ea01dc5029e94e9d882
lior <liorsiag@gmail.com>
2022-07-09 21:51:43 +0300

    Fixed GAP heuristic and added half-gap support

--- domains/pancakes.py
+++ domains/pancakes.py
@@ -13,27 +13,38 @@ class PancakesState(DomainState):
 
 class Pancakes(Domain):
     def __init__(self,
-                 ignore_pancakes_up_to=0,
+                 ignore_pancakes_up_to: float = 0,
                  size=0,
                  initial_state: PancakesState = None):
         if initial_state is not None:
             self.goal_state = PancakesState(stack=tuple(sorted(initial_state.stack, reverse=True)))
             self.size = len(initial_state.stack)
         elif size > 0:
-            self.goal_state = PancakesState(stack=tuple(range(size - 1, -1, -1)))
+            self.goal_state = PancakesState(stack=tuple(range(size, 0, -1)))
             self.size = size
         else:
             raise Exception('Must provide initial state or stack size')
-        # If we want to ignore 0 pancakes, we want to include all, but if we want to ignore 2, we
-        # want to start the loop from 3, therefore, if it's bigger than 0, we add 1
-        self.start_count_from = ignore_pancakes_up_to + (ignore_pancakes_up_to > 0)
+
+        self.half_gap = False
+        if ignore_pancakes_up_to == int(ignore_pancakes_up_to):
+            self.ignore_pancakes_up_to = int(ignore_pancakes_up_to)
+        else:
+            if ignore_pancakes_up_to - int(ignore_pancakes_up_to) == 0.5:
+                self.ignore_pancakes_up_to = int(ignore_pancakes_up_to) + 1
+                self.half_gap = True
+            else:
+                raise Exception("If fraction is given, it has to be 0.5 for half gaps")
 
     def heuristic(self, state):
         # In this formula, we always add 1 if the max pancake is not at the bottom.
-
-        # Consult someone how to treat this case.
-        gaps = sum(abs(state.stack[i] - state.stack[i + 1]) > 1 for i in
-                   range(self.start_count_from, self.size - 1))
+        gaps = 0
+        for i in range(len(state.stack) - 1):
+            if state.stack[i] > self.ignore_pancakes_up_to and state.stack[i + 1] > self.ignore_pancakes_up_to \
+                    and abs(state.stack[i] - state.stack[i + 1]) > 1:
+                gaps += 1
+            elif self.half_gap and state.stack[i] == self.ignore_pancakes_up_to and \
+                    state.stack[i + 1] > self.ignore_pancakes_up_to:
+                gaps += 1
         return gaps + (state.stack[0] != max(state.stack))
 
     def goal_test(self, state):
@@ -46,21 +57,10 @@ class Pancakes(Domain):
         instances = []
         for instance_num in range(num_instances):
             num_ops = random.randrange(min_ops, max_ops + 1)
-            stack = list(range(self.size))  # Start from the solved state
+            stack = list(range(1, self.size + 1))  # Start from the solved state
             stack.reverse()
             for i in range(num_ops):
                 flip_i = random.randrange(0, self.size - 1)
                 stack = stack[0:flip_i] + stack[flip_i:self.size][::-1]
             instances.append(PancakesState(tuple(stack)))
         return instances
-
-
-def main():
-    domain = Pancakes(size=20)
-    d = domain.generate_instances(10, 1, 1)
-    for i in d:
-        instance_str = ' '.join(str(pancake) for pancake in i.stack)
-
-
-if __name__ == '__main__':
-    main()
