7f1b580730322cc7307e2818ac23bdc604bea7d4
lior <liorsiag@gmail.com>
2022-07-10 10:47:02 +0300

    Added astar search

--- search/astar_searcher.py
+++ search/astar_searcher.py
@@ -6,25 +6,18 @@ from tqdm import tqdm
 from search.searcher import Searcher, SearchNode, Timeout, NoSolution
 
 
-class PotentialSearcher(Searcher):
-    def solve(self, init_state, c, pure_heuristic_search=False, timeout=60, quiet=False):
-        # If we are dealing with pure heuristic search f(n)=h(n), in the case of potential search f(n)=u(n)
-        # Which means the used formula (smaller is better)
-        def calc_priority(g, h):
-            if pure_heuristic_search:
-                return h
-            else:
-                return h / (c - g)
-
+class AstarSearcher(Searcher):
+    def solve(self, init_state, timeout=60, quiet=False):
+        self.reset_stats()
         start_time = time.time()
         root_h = self.domain.heuristic(init_state)
-        root = SearchNode(calc_priority(0, root_h), root_h, 0, init_state)
+        root = SearchNode(root_h, root_h, 0, init_state)
         self.generated += 1
         # This might be somewhat counter-intuitive, but we update closed alongside open, since we cannot search in
         # O(1) in a priority queue, that is why we have an in_open field in SearchNode
         closed = {root.state: root}
         open_ = [root]
-        with tqdm(disable=quiet) as pbar:  # Progress bar (helps to see search speed)
+        with tqdm(disable=quiet) as pbar:
             while open_:
                 # Check for timeouts
                 if time.time() - start_time > timeout:
@@ -39,10 +32,14 @@ class PotentialSearcher(Searcher):
                 if not node.is_valid:
                     continue
 
+                if self.domain.goal_test(node.state):
+                    self.total_time = time.time() - start_time
+                    self.cost = node.g
+                    return self.cost, self.total_time
+
                 self.expanded += 1
                 pbar.update(1)
 
-                # Iterate over the neighbors
                 for (neighbor, cost_to) in self.domain.get_successors_and_op_cost(node.state):
                     g_neighbor = node.g + cost_to
 
@@ -53,19 +50,10 @@ class PotentialSearcher(Searcher):
                         continue
 
                     h_neighbor = self.domain.heuristic(neighbor)
-                    # If the f(n) of the node is larger or equal to the cost bound, we discard it.
-                    if g_neighbor + h_neighbor >= c:
-                        continue
-
-                    # Check if it's the goal, and we already know the path cost is under the cost bound
-                    if self.domain.goal_test(neighbor):
-                        self.total_time = time.time() - start_time
-                        self.cost = g_neighbor
-                        return self.cost, self.total_time
 
                     # If it not the goal, that means we are going to either insert a new node into open, or "update" an
                     # already existing node
-                    new_node = SearchNode(calc_priority(g_neighbor, h_neighbor), h_neighbor, g_neighbor, neighbor)
+                    new_node = SearchNode(g_neighbor + h_neighbor, h_neighbor, g_neighbor, neighbor)
                     self.generated += 1
 
                     # If we have already seen this node before
@@ -83,5 +71,5 @@ class PotentialSearcher(Searcher):
                     closed[neighbor] = new_node
                     heapq.heappush(open_, new_node)
 
-        self.total_time = time.time() - start_time
-        raise NoSolution(f"No solution within bound {c}. Elapsed time: {self.total_time} seconds.")
+            self.total_time = time.time() - start_time
+            raise NoSolution(f"No solution within bound {c}. Elapsed time: {self.total_time} seconds.")
--- search/searcher.py
+++ search/searcher.py
@@ -1,6 +1,21 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
 
-from domains.domain import Domain
+from domains.domain import Domain, DomainState
+
+
+@dataclass(unsafe_hash=True, order=True)  # Not frozen to allow changing h and, consequently, f.
+# The members that participate in the hash are immutable.
+class SearchNode:
+    f: float = field(hash=False)  # Order of fields determines comparison order
+    h: float = field(hash=False)  # Tie-break in favor of smaller h -> higher g.
+    g: float = field(hash=False)
+    state: DomainState  # It does participate in __cmp__ (and TilePuzzleState is defined with the default
+    # order=True) only to make tie-breaking deterministic
+    parent: "SearchNode" = field(default=None, repr=False, hash=False, compare=False)  # The string is a forward
+    # reference
+    in_open: bool = field(default=True, repr=False, hash=False, compare=False)
+    is_valid: bool = field(default=True, repr=False, hash=False, compare=False)
 
 
 class Searcher(ABC):
@@ -12,7 +27,7 @@ class Searcher(ABC):
     total_time: float
 
     def __init__(self, domain: Domain):
-        self._domain = domain
+        self.domain = domain
         self.expanded = 0
         self.generated = 0
         self.reopened = 0
